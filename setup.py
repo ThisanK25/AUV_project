dbca0ccad060aab762ed6d558753ee8acb2552b5
Jonas <jonaen@ifi.uio.no>
2024-10-28 13:43:03 +0100

    setup tools

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AUV_Project',
+    version='0.1.0',
+    description='in5490 project 4',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jonas E. Wenberg, Sigmund, Thisan',
+    author_email='jonaen@üio.no',
+    url='https://github.com/ThisanK25/AUV_project',  # or the URL of your project
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'xarray', 'scipy', 'matplotlib'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',  # Specify the Python versions you support
+)
