2c90ab7a313014257bb938ab8dd1eff1398ad572
Jonas <jonaen@ifi.uio.no>
2024-11-06 22:30:50 +0100

    matte e vanskeli

--- src/Q_simulator.py
+++ src/Q_simulator.py
@@ -88,7 +88,7 @@ def fetch_sim_files(directory = Path(r"./sim")) -> filter:
 
 
 if __name__ == "__main__":
-    with tqdm(total=len(list(fetch_sim_files())) * 5, ncols=100, desc="Reading files", bar_format='\033[0m{l_bar}{bar} \033[91m [elapsed: {elapsed} remaining: {remaining}]', colour='red') as pbar:
+    with tqdm(total=len(list(fetch_sim_files())) * 6, ncols=100, desc="Reading files", bar_format='\033[0m{l_bar}{bar} \033[91m [elapsed: {elapsed} remaining: {remaining}]', colour='red') as pbar:
         for file_path in fetch_sim_files():
             for depth in range(64, 70):
                 env   = Q_Environment(file_path, depth=depth)
